5075772115405ecf9ead09dc642f163509381fff
muzammil6292 <159890890+muzammil6292@users.noreply.github.com>
2025-01-08 03:46:29 +0000

    any name about project

--- day4.py
+++ day4.py
@@ -1,10 +1,13 @@
-str_1="hello "
-str_2=" world......"
-res=str_1+"()" +str_2
-print(res)
-stt="Python"
-result=stt*3
-print(result)
-strr="hello wooorrlldd"
-sub=strr[4:7]
-print(sub)
+def sum_even_numbers(n):
+   
+    if n < 1:
+        return "Please enter a positive integer greater than or equal to 1."
+    ev_sum = sum(i for i in range(2, n + 1, 2))
+    return ev_sum
+
+try:
+    n = int(input("Enter a positive integer: "))
+    result = sum_even_numbers(n)
+    print(f"The sum of all even numbers between 1 and {n} is: {result}")
+except ValueError:
+    print("Invalid input! Please enter a positive integer.")
